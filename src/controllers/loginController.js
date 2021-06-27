a0b2bfbfe444ab7ad2b826fff50ada3a9775e553
samuel <samuellyworld@gmail.com>
2021-06-27 11:48:20 -0700

    fixed bugs and added sessions

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,37 +1,57 @@
 const Employer = require("../models/employer");
 const Student = require("../models/student");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+// handle sign in
+const handleStudentLogin =(req, res) => {
+  // check for request body validation
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "all fields can not be empty"
+      });
+    }
 
-exports.studentLogin = (req, res) => {
-  const { email, password } = req.body
-  if (!email || !password) {
-    return res.status(400).json({
-      message: "all fields can not be empty"
-    });
+    Student.findOne({
+      email: email
+    })
+      .exec((err, student) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        // compare the password of the req.body and the one in the database
+        const passwordIsValid = bcrypt.compareSync(password, student.password);
+        if (!passwordIsValid) {
+          return res.status(401).send({
+            message: "Invalid Password!"
+          });
+        }
+        if (passwordIsValid) {
+          // get the student profile
+          return res.status(200).send(student)
+        } else {
+          Promise.reject('wrong credentials');
+        }
+      })
   }
 
-  Student.findOne({
-    email: email
-  })
-    .exec((err, student) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-      // compare the password of the req.body and the one in the database
-      const passwordIsValid = bcrypt.compareSync(password, student.password);
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          message: "Invalid Password!"
-        });
-      }
-      if (passwordIsValid) {
-        // get the student profile
-        return res.status(200).send(student)
-      } else {
-        Promise.reject('wrong credentials');
-      }
-    })
+  // get jwt authorization token
+  const getAuthTokenId = (req,res) => {
+    const {authorization} = req.headers;
+   return jwt.verify(authorization, 'secret', (err, reply) =>{
+        if(err || !reply) {
+          return res.status(400).json('Unauthorized');
+        }
+        return res.json({id: reply});
+    }
+     )
+  }
 
+
+
+exports.studentLoginAuthentication = (req, res) => {
+  const{ authorization} = req.headers;
+    return authorization ? getAuthTokenId(req,res) : handleStudentLogin(req,res)
 }
